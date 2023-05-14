482782a0c36f19752fd7e590b7f3a2740652f3c7
jungheeeun <jungheeeun272@gmail.com>
2023-05-14 17:13:35 +0900

    createServer 딜리버리 다른 패턴도 보여주게끔 처리

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,27 +1,3 @@
-/**
- * * index.js는 앱 서버로 사용하기로 결정했다.
- * * app.js를 import 하는 형태
- * 
- * 
- * 
- * 
- */
-
-
-/**
- * 
- * * 설명
- * * css '작성 방식'을 번들링에서 사용하려면
- * * css-loader, style-loader가 필요하다.
- * * 해당 entry point 파일에 css 파일을 import 해야한다.
- * * 굳이 변수가 필요없으므로, require() commonJS 방식으로 읽어 들였다.
- */
-require("./assets/css/index.css");
-require("./assets/css/bundle-test-1.css");
-
-const http = require('http');
-const app = require('./app');
-const port = 1234;
-
-
+console.log('hello');
+const a = 1;
 
--- src/index.js
+++ src/index.js
@@ -6,3 +6,23 @@
  * 
  * 
  */
+
+/**
+ * 
+ * * 설명
+ * * css '작성 방식'을 번들링에서 사용하려면
+ * * css-loader, style-loader가 필요하다.
+ * * 해당 entry point 파일에 css 파일을 import 해야한다.
+ * * 굳이 변수가 필요없으므로, require() commonJS 방식으로 읽어 들였다.
+ */
+require("./assets/css/index.css");
+require("./assets/css/bundle-test-1.css");
+
+const http = require('http');
+const app = require('./app');
+const port = 1234;
+
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`서버가 여기에서 ${port} 돌아간다 에헤라디야`);
+});
